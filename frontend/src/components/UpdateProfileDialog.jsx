61cdd7064f870cd360031f008205b526d704f9fa
Shikhar singh <146853377+singhshikhar016@users.noreply.github.com>
2025-04-27 16:57:03 +0530

    Update UpdateProfileDialog.jsx

--- frontend/src/components/UpdateProfileDialog.jsx
+++ frontend/src/components/UpdateProfileDialog.jsx
@@ -46,7 +46,7 @@ const UpdateProfileDialog = ({ open, setOpen }) => {
         }
         try {
             setLoading(true);
-            const res = await axios.post(`${USER_API_END_POINT}/profile/update`, formData, {
+            const res = await axios.post(`https://job-portal-final-1.vercel.app/profile/update`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 },
