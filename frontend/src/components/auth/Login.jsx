1919c615eefe00858817eadbdca3cf4bff5486fe
Shikhar singh <146853377+singhshikhar016@users.noreply.github.com>
2025-04-27 16:47:29 +0530

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
         e.preventDefault();
         try {
             dispatch(setLoading(true));
-            const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+            const res = await axios.post(`https://job-portal-final-1.vercel.app//login`, input, {
                 headers: {
                     "Content-Type": "application/json"
                 },
