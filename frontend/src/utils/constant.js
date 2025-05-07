6770efc55d9ed401b71182c2c2a42357cd9c6e29
Shikhar singh <146853377+singhshikhar016@users.noreply.github.com>
2025-05-07 11:21:53 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT="https://job-portal-final-1.vercel.app/api/v1/user";
-export const JOB_API_END_POINT="https://job-portal-final-1.vercel.app/v1/job";
-export const APPLICATION_API_END_POINT="https://job-portal-final-1.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-portal-final-1.vercel.app/api/v1/company";
+export const USER_API_END_POINT="https://job-portal-backend-seven-omega.vercel.app/user";
+export const JOB_API_END_POINT="https://job-portal-backend-seven-omega.vercel.app/v1/job";
+export const APPLICATION_API_END_POINT="hhttps://job-portal-backend-seven-omega.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-portal-backend-seven-omega.vercel.app/api/v1/company";
+
